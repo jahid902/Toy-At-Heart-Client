ab14f5b005e93096659d00c089492de230ff9e9b
jahid902 <jahidjr902@gmail.com>
2023-05-20 21:02:12 +0600

    fixed google image issue

--- src/components/SharedPages/SocialButton.jsx
+++ src/components/SharedPages/SocialButton.jsx
@@ -33,7 +33,7 @@ const SocialButton = () => {
       <div className="grid h-14 card rounded-box place-items-center bg-orange-100 hover:bg-orange-300 duration-500">
         <img
           onClick={GoogleSignIn}
-          src="/public/google (1).png"
+          src="/google (1).png"
           className="w-8 h-8  cursor-pointer"
           alt=""
         />
